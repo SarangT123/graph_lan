71924e00dcd7495b2b81bc7e28eabbc27096798d
Sarangt123 (sarangt123) <sarang.thekkedathpr@gmail.com>
2021-12-23 17:58:10 +0530

    Typo fix

--- graph_lan/__main__.py
+++ graph_lan/__main__.py
@@ -1,7 +1,13 @@
 import sys
 import compiler
-if len(sys.argv) < 4:
+if len(sys.argv) < 3:
     print("Usage: python3 main.py -r <filename> <compiled or interpreted(-i/-c)>")
     sys.exit(1)
 if sys.argv[1] == '-r':
-    compiler.run(sys.argv[2]).run()
+    if len(sys.argv) >= 4:
+        if sys.argv[3] == '-R':
+            compiler.run(sys.argv[2]).run()
+            with open("compiled.py", "r") as f:
+                exec(f.read())
+    else:
+        compiler.run(sys.argv[2]).run()
